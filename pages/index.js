0b60ad82f15ba40ef3728e8e8152d4546896d9d4
WarmGlow <emma.spencer@thelifeyoucansave.org>
2022-09-05 23:28:51 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -178,138 +178,59 @@ export default function Home() {
             />
           </div>
           <div class="flex justify-center">
-            <div>
-              <div class="dropdown relative">
-                <button
-                  className="
-                    dropdown-toggle
-                    px-6
-                    py-2.5
-                    bg-blue-600
-                    text-white
-                    font-medium
-                    text-xs
-                    leading-tight
-                    uppercase
-                    rounded
-                    shadow-md
-                    hover:bg-blue-700 hover:shadow-lg
-                    focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-                    active:bg-blue-800 active:shadow-lg active:text-white
-                    transition
-                    duration-150
-                    ease-in-out
-                    flex
-                    items-center
-                    whitespace-nowrap
-                  "
-                  type="button"
-                  id="dropdownMenuButton1"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown button
-                  <svg
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fas"
-                    data-icon="caret-down"
-                    class="w-2 ml-2"
-                    role="img"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 320 512"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
-                    ></path>
-                  </svg>
-                </button>
-                <ul
-                  className="
-                    dropdown-menu
-                    min-w-max
-                    absolute
-                    hidden
-                    bg-white
-                    text-base
-                    z-50
-                    float-left
-                    py-2
-                    list-none
-                    text-left
-                    rounded-lg
-                    shadow-lg
-                    mt-1
-                    hidden
-                    m-0
-                    bg-clip-padding
-                    border-none
-                  "
-                  aria-labelledby="dropdownMenuButton1"
-                >
-                  <li>
-                    <a
-                      className="
-                        dropdown-item
-                        text-sm
-                        py-2
-                        px-4
-                        font-normal
-                        block
-                        w-full
-                        whitespace-nowrap
-                        bg-transparent
-                        text-gray-700
-                        hover:bg-gray-100
-                      "
-                      href="#"
-                      >Action</a
-                    >
-                  </li>
-                  <li>
-                    <a
-                      className="
-                        dropdown-item
-                        text-sm
-                        py-2
-                        px-4
-                        font-normal
-                        block
-                        w-full
-                        whitespace-nowrap
-                        bg-transparent
-                        text-gray-700
-                        hover:bg-gray-100
-                      "
-                      href="#"
-                      >Another action</a
-                    >
-                  </li>
-                  <li>
-                    <a
-                      className="
-                        dropdown-item
-                        text-sm
-                        py-2
-                        px-4
-                        font-normal
-                        block
-                        w-full
-                        whitespace-nowrap
-                        bg-transparent
-                        text-gray-700
-                        hover:bg-gray-100
-                      "
-                      href="#"
-                      >Something else here</a
-                    >
-                  </li>
-                </ul>
-              </div>
+            <div class="mb-3 xl:w-96">
+              <select class="form-select appearance-none
+                block
+                w-full
+                px-3
+                py-1.5
+                text-base
+                font-normal
+                text-gray-700
+                bg-white bg-clip-padding bg-no-repeat
+                border border-solid border-gray-300
+                rounded
+                transition
+                ease-in-out
+                m-0
+                focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+                  <option selected>Pick a Charity --</option>
+                  <option value="1">charity_1</option>
+                  <option value="2">charity_2</option>
+                  <option value="3">charity_3</option>
+              </select>
             </div>
           </div>
           <div className="mb-6">
+            <div class="flex justify-center">
+  <div class="mb-3 xl:w-96">
+              <label for="exampleNumber0" class="form-label inline-block mb-2 text-gray-700"
+                >Donation Amount ($)</label
+              >
+                <input
+                  type="number"
+                  class="
+                    form-control
+                    block
+                    w-full
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                  "
+                  id="exampleNumber0"
+                  placeholder="Number input"
+                />
+              </div>
+            </div>
             <label className="block text-sm text-[#535353] mb-2">Write a Personal Message:</label>
             <textarea
               rows="4"
